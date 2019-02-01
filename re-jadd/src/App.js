885c973ea03ef1995a847968ad04c72032aa58a3
Masarah Fallath <masarah@MacBook-Air-alkhas-b-Masarah.local>
2019-02-01 20:50:15 +0300

    react

--- re-jadd/src/App.js
+++ re-jadd/src/App.js
@@ -1,14 +1,77 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-
-
+import React, { Component } from "react";
+import { getUser, logout } from "./services/authService";
+import NavBar from "./components/NavBar";
+import AuthForm from "./components/AuthForm";
+import Profile from "./components/Profile";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      form: "signup"
+    };
+  }
+
+  checkForUser() {
+    const user = getUser();
+    if (user) {
+      this.setState({ user });
+    }
+  }
+  componentDidMount() {
+    this.checkForUser();
+  }
+
+  changeForm = type => {
+    console.log(type);
+    this.setState({
+      form: type
+    });
+  };
+
+  login = () => {
+    const user = getUser();
+    this.setState({ user });
+  };
+
+  logout = () => {
+    logout();
+    this.setState({ user: null });
+  };
+
+  getProducts = () => { };
+
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <header>
+          <img src="" />
+          <NavBar
+            user={this.state.user}
+            changeForm={this.changeForm}
+            logout={this.logout}
+            getProducts={this.getProducts}
+          />
+        </header>
+        <div className="home">
+
+          <div className="container">
+            {this.state.user ? (
+              <div className="profile"> <Profile user={this.state.user} /></div>
+            ) : (<div>
+              <AuthForm form={this.state.form} onLogin={this.login} />
+              <div >
+
+                {!this.state.user && (<div className="bttn" onClick={() => this.changeForm("signup")}> <h3>Start Recycling</h3></div>)}
+              </div>
+            </div>
+              )}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ re-jadd/src/components/Signup.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Signup = ({ renderInput, handleSubmit }) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {renderInput("name", "Name")}
+        {renderInput("email", "Email")}
+        {renderInput("password", "Password", "password")}
+        {renderInput("phone", "Phone")}
+        {renderInput("location", "Location")}
+
+        <button className="btn btn-primary"> SignUp </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
